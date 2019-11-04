b49c6ffeb8166478edb880ca98b1f956a49fa5d3
Juliano Vaz <jctvEC@github.com>
2019-11-04 16:20:30 -0300

    colocando header e footer

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <Header/>
+    </div>
+    <div>
+      <img alt="Vue logo" src="./assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+    <div>
+      <Footer/>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Footer from './components/pages/shared/Footer.vue'
+import Header from './components/pages/shared/Header.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Footer,
+    Header
   }
 }
 </script>
@@ -23,6 +35,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
