088c3a1a48e170dc7f7bdb4e1724bee9ab5b3ff9
juliano <jctv@cin.ufpe.br>
2019-11-15 15:56:10 -0300

    Componente Buscador
    falta colocar o componente de localizacao que ta comentando, feito por Sergio

--- app/src/components/pages/Buscador.vue
+++ app/src/components/pages/Buscador.vue
@@ -1,23 +1,51 @@
 <template>
-  <div>
+  <div class="geral">
+    <!--  href="https://drive.google.com/open?id=1Eu__OmZCIjtVQjEmNCvREDY-6BzZIAql&usp=sharing" -->
+    <!-- Buscador colocar esse link dentro de um componenet para ver a localizacao -->
 
-        <!--  href="https://drive.google.com/open?id=1Eu__OmZCIjtVQjEmNCvREDY-6BzZIAql&usp=sharing" -->
-  Buscador
+    <b-form inline>
+      <label class="sr-only" for="inline-form-input-name">Name</label>
+      <b-input
+        id="inline-form-input-name"
+        class="buscador mb-2 mr-sm-2 mb-sm-0"
+        placeholder="Digite aqui"
+      ></b-input>
+
+      <b-button variant="secondary" style="margin-left:2%">Pesquisar</b-button>
+    </b-form>
+    <b-form inline>
+      <input type="radio" id="nome" value="localizacao" name="grupo" />
+      <label for="localizacao">Nome/Título</label>
+      <br />
+      <input type="radio" id="local" value="localizacao" name="grupo" />
+      <label for="localizacao">Localização</label>
+      <br />
+      <input type="radio" id="tematica" value="tematica" name="grupo" />
+      <label for="tematica">Causa/Temática/Tag</label>
+      <br />
+    </b-form>
   </div>
 </template>
 
-
 <script>
-
-
 export default {
-  name: 'Buscador',
-}
-
+  name: "Buscador"
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="stylus" scoped>
+<style scoped>
+.geral {
+  margin-top: 23%;
+  margin-left: 15%;
+  overflow: hidden; /* tirar barrar de scrolling */
+}
 
+input {
+  margin-left: 2%;
+}
+
+.buscador {
+  padding-right: 50%;
+}
 </style>
-
