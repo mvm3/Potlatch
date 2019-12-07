30ab9a0fbc652f1b1c8aba615c5282939b3b8c0a
Juliano Vaz <jctvEC@github.com>
2019-12-07 12:02:25 -0300

    Descubra

---
+++ app/src/components/pages/Descubra.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    Eu existo
+    </div>
+</template>
+
+
+<script>
+
+
+export default {
+  name: 'Descubra',
+  components:
+  {
+
+  }
+
+
+}
+</script>
+
+<style  scoped>
+
+</style>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
